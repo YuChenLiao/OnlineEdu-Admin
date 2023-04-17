2a9b4bb385ca650ae1780669e8ce6bccc64e1b08
yuchenliao <yuchenliao3@gmail.com>
2023-04-17 16:58:59 +0800

    解决无法报错的问题

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="container">
-    <div class="logo">
-      <img alt="logo"
-        src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/dfdba5317c0c20ce20e64fac803d52bc.svg~tplv-49unhts6dw-image.image" />
-      <div class="logo-text">Arco Design Pro</div>
-    </div>
-    <LoginBanner />
     <div class="content">
       <div class="content-inner">
         <LoginForm />
@@ -19,7 +13,6 @@
 
 <script lang="ts" setup>
 import Footer from '@/components/footer/index.vue';
-import LoginBanner from './components/banner.vue';
 import LoginForm from './components/login-form.vue';
 </script>
 
@@ -27,11 +20,6 @@ import LoginForm from './components/login-form.vue';
 .container {
   display: flex;
   height: 100vh;
-
-  .banner {
-    width: 550px;
-    background: linear-gradient(163.85deg, #1d2129 0%, #00308f 100%);
-  }
 
   .content {
     position: relative;
@@ -49,31 +37,5 @@ import LoginForm from './components/login-form.vue';
     width: 100%;
   }
 }
-
-.logo {
-  position: fixed;
-  top: 24px;
-  left: 22px;
-  z-index: 1;
-  display: inline-flex;
-  align-items: center;
-
-  &-text {
-    margin-right: 4px;
-    margin-left: 4px;
-    color: var(--color-fill-1);
-    font-size: 20px;
-  }
-}
 </style>
 
-<style lang="less" scoped>
-// responsive
-@media (max-width: @screen-lg) {
-  .container {
-    .banner {
-      width: 25%;
-    }
-  }
-}
-</style>
